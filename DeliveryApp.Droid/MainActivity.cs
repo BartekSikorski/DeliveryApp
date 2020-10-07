d332c555141fc42f20be870f03cced0be6ee7548
Bartek Sikorski <bartek.sikorski@gmail.com>
2020-10-07 22:00:03 +0200

    Login activity

--- DeliveryApp.Droid/MainActivity.cs
+++ DeliveryApp.Droid/MainActivity.cs
@@ -9,13 +9,34 @@ namespace DeliveryApp.Droid
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText emailEditText, passwordEditText;
+        Button signinButtom, registerButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
+            emailEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
+            signinButtom = FindViewById<Button>(Resource.Id.signinButton);
+
+            registerButton = FindViewById<Button>(Resource.Id.registerButton);
+
+            signinButtom.Click += SigninButtom_Click;
+            registerButton.Click += RegisterButton_Click;
+
         }
+
+        private void RegisterButton_Click(object sender, System.EventArgs e)
+        {
+        }
+
+        private void SigninButtom_Click(object sender, System.EventArgs e)
+        {
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
